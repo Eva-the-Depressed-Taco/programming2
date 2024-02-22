2d35cc55b141498efd26b1cefbe738d8b33b3c80
Eva-the-Depressed-Taco <white.e@stu.janesville.k12.wi.us>
2024-02-22 14:34:47 -0600

    54c
    yup

---
+++ p54c/p54c/MainForm.py
@@ -0,0 +1,58 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._label1 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(31, 145)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.TabIndex = 0
+		self._button1.Text = "circle calc"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# textBox1
+		# 
+		self._textBox1.Location = System.Drawing.Point(31, 112)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(100, 20)
+		self._textBox1.TabIndex = 1
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.FromArgb(255, 192, 255)
+		self._label1.Location = System.Drawing.Point(31, 186)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(100, 23)
+		self._label1.TabIndex = 2
+		self._label1.Text = "label1"
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(284, 261)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "p54c"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button1Click(self, sender, e):
+		r = int(self._textBox1.Text)
+		A = r * 3.14
+		self._label1.Text = str(A)
