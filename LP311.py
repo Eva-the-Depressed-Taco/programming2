8bf629b0a602aa3360e322e1e69c21122d7e436c
Eva-the-Depressed-Taco <143825525+Eva-the-Depressed-Taco@users.noreply.github.com>
2024-03-04 20:11:33 +0000

    435435

---
+++ LP311.py
@@ -0,0 +1,15 @@
+des = int(input("enter designing "))
+cod = int(input("enter designing "))
+deb = int(input("enter designing "))
+tes = int(input("enter designing "))
+
+time = des + cod + deb + tes
+desP = des / time
+codP = cod / time
+debP = deb / time
+tesP = tes / time
+print ("percntage diplayed out")
+print("design percntage  =", desP)
+print("codeing percntage  =", codP)
+print("debug percntage  =", debP)
+print("test percntage  =", tesP)
