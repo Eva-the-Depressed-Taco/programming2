30b4f940c8002c55e13b714a501d36cacf0a188f
Eva-the-Depressed-Taco <143825525+Eva-the-Depressed-Taco@users.noreply.github.com>
2024-03-29 20:23:17 +0000

    top sneaky

--- beecube.py
+++ beecube.py
@@ -1,18 +1,25 @@
-unit = input("Enter a unit, millimeters, meters, killometers, lightyears: ")
-  if unit == millimeters:
-    millimeters = int(input("Enter Length")
-                      bees = millimeters / 30
-                      bees**3
-                      
-  if unit == meters:
-    meters = int(input("Enter Length")
-  if unit == killometers:
-                 
-    killometers = int(input("Enter Length")
-  if unit == lightyears:
-    lightyears = int(input("Enter Length")
+caltype = input("would you like to enter a base length or number of bees? length/bees: ")
+if caltype == "length":
+  unit = input("Enter a unit, millimeters, meters, kilometers, lightyears: ")
 
-meter = 1000 * millimeters
-killometer = 1000 * meters
-lightyear = 9460730.5 * killometer
-bee = 30 millimeter
+  if unit == "millimeters":
+      millimeters = int(input("Enter Length: "))
+      beeL = millimeters / 30
+
+  elif unit == "meters":
+      meters = int(input("Enter Length: "))
+      beeL = meters * 1000 / 30
+
+  elif unit == "kilometers":
+      kilometers = int(input("Enter Length: "))
+      beeL = kilometers * 1000 * 1000 / 30
+
+  elif unit == "lightyears":
+      lightyears = int(input("Enter Length: "))
+      beeL = lightyears * 9460730472600 * 1000 * 1000 / 30
+
+  bees = beeL ** 3
+  print("Number of bees:", bees)
+  #print("Cubed bees:", beeL)
+elif caltype == "bees":
+  print("feature unavalibe:")
