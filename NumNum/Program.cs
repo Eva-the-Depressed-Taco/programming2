d9f281cc099114d0ac595bb787f53030f6780960
Eva-the-Depressed-Taco <143825525+Eva-the-Depressed-Taco@users.noreply.github.com>
2024-04-16 14:12:37 -0500

    Update

---
+++ NumNum/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: white.e
+ * Date: 4/16/2024
+ * Time: 2:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NumNum
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
