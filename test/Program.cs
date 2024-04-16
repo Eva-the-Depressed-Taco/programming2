1b01d91a5307485be7a20054636678a7c161d558
Eva-the-Depressed-Taco <143825525+Eva-the-Depressed-Taco@users.noreply.github.com>
2024-04-16 14:07:56 -0500

    Update

---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: white.e
+ * Date: 4/16/2024
+ * Time: 2:07 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Test");
+			Console.ReadKey(true);
+		}
+	}
+}
