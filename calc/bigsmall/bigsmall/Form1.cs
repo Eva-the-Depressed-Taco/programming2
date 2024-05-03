260cb5947f8aa467f3cda887d0c86dfa41e3734e
Eva-the-Depressed-Taco <143825525+Eva-the-Depressed-Taco@users.noreply.github.com>
2024-05-03 14:25:22 -0500

    bigsmall

---
+++ calc/bigsmall/bigsmall/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bigsmall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            if (n1 == n2)
+                label1.Text = ("nums are equal");
+            else if (n1 > n2)
+                label1.Text = n1.ToString();
+            else
+                label1.Text = n2.ToString();
+
+
+        }
+    }
+}
